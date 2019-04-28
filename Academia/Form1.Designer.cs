877b32f0e25b9fedb94f4607b7eea201c335db7d
Otoniel <otoniel.candido47@gmail.com>
2019-04-28 19:16:32 -0300

    Tela de login

---
+++ Academia/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Academia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbEmail = new System.Windows.Forms.TextBox();
+            this.txbSsenha = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnMinimizar = new System.Windows.Forms.Label();
+            this.btnFechar = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chkVisualizarSenha = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txbEmail
+            // 
+            this.txbEmail.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txbEmail.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbEmail.ForeColor = System.Drawing.Color.Silver;
+            this.txbEmail.Location = new System.Drawing.Point(112, 132);
+            this.txbEmail.Name = "txbEmail";
+            this.txbEmail.Size = new System.Drawing.Size(291, 29);
+            this.txbEmail.TabIndex = 0;
+            this.txbEmail.Text = "Nome";
+            this.txbEmail.Enter += new System.EventHandler(this.txbEmail_Enter);
+            this.txbEmail.Leave += new System.EventHandler(this.txbEmail_Leave);
+            // 
+            // txbSsenha
+            // 
+            this.txbSsenha.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txbSsenha.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbSsenha.ForeColor = System.Drawing.Color.Silver;
+            this.txbSsenha.Location = new System.Drawing.Point(112, 173);
+            this.txbSsenha.Name = "txbSsenha";
+            this.txbSsenha.Size = new System.Drawing.Size(291, 29);
+            this.txbSsenha.TabIndex = 1;
+            this.txbSsenha.Text = "Senha";
+            this.txbSsenha.Enter += new System.EventHandler(this.txbSsenha_Enter);
+            this.txbSsenha.Leave += new System.EventHandler(this.txbSsenha_Leave);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(176, 243);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 41);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Entrar";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // btnMinimizar
+            // 
+            this.btnMinimizar.AutoSize = true;
+            this.btnMinimizar.BackColor = System.Drawing.Color.Transparent;
+            this.btnMinimizar.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinimizar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnMinimizar.Location = new System.Drawing.Point(453, 0);
+            this.btnMinimizar.Name = "btnMinimizar";
+            this.btnMinimizar.Size = new System.Drawing.Size(17, 23);
+            this.btnMinimizar.TabIndex = 5;
+            this.btnMinimizar.Text = "-";
+            this.btnMinimizar.Click += new System.EventHandler(this.label3_Click);
+            this.btnMinimizar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnMinimizar_MouseDown);
+            this.btnMinimizar.MouseEnter += new System.EventHandler(this.btnMinimizar_MouseEnter);
+            this.btnMinimizar.MouseLeave += new System.EventHandler(this.btnMinimizar_MouseLeave);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.AutoSize = true;
+            this.btnFechar.BackColor = System.Drawing.Color.Transparent;
+            this.btnFechar.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFechar.Location = new System.Drawing.Point(470, 0);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(20, 23);
+            this.btnFechar.TabIndex = 6;
+            this.btnFechar.Text = "x";
+            this.btnFechar.Click += new System.EventHandler(this.label4_Click);
+            this.btnFechar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnFechar_MouseDown);
+            this.btnFechar.MouseEnter += new System.EventHandler(this.btnFechar_MouseEnter);
+            this.btnFechar.MouseLeave += new System.EventHandler(this.btnFechar_MouseLeave);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BackgroundImage = global::Academia.Properties.Resources.iconeUsuario;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(84, 132);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(29, 29);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.BackgroundImage = global::Academia.Properties.Resources.iconeSenha;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(84, 173);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(29, 29);
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(170, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 18);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Csharp Academy";
+            // 
+            // chkVisualizarSenha
+            // 
+            this.chkVisualizarSenha.AutoSize = true;
+            this.chkVisualizarSenha.BackColor = System.Drawing.Color.Transparent;
+            this.chkVisualizarSenha.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkVisualizarSenha.Location = new System.Drawing.Point(279, 204);
+            this.chkVisualizarSenha.Name = "chkVisualizarSenha";
+            this.chkVisualizarSenha.Size = new System.Drawing.Size(122, 19);
+            this.chkVisualizarSenha.TabIndex = 8;
+            this.chkVisualizarSenha.Text = "Visualizar senha";
+            this.chkVisualizarSenha.UseVisualStyleBackColor = false;
+            this.chkVisualizarSenha.CheckedChanged += new System.EventHandler(this.chkVisualizarSenha_CheckedChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Constantia", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(1, 335);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Versão Apha 1.0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::Academia.Properties.Resources.WallpaperLogin3_jpg;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(490, 351);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.chkVisualizarSenha);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnMinimizar);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txbSsenha);
+            this.Controls.Add(this.txbEmail);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbEmail;
+        private System.Windows.Forms.TextBox txbSsenha;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label btnMinimizar;
+        private System.Windows.Forms.Label btnFechar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox chkVisualizarSenha;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Academia/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Academia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void chkVisualizarSenha_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkVisualizarSenha.Enabled == true && txbSsenha.PasswordChar == '*')
+                 txbSsenha.PasswordChar = '\0';
+            else if (txbSsenha.Text != "Senha")
+                 txbSsenha.PasswordChar = '*';
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void txbEmail_Enter(object sender, EventArgs e)
+        {
+            if (txbEmail.Text == "Nome")
+            {
+                txbEmail.Text = string.Empty;
+                txbEmail.ForeColor = Color.Black;
+            }
+        }
+
+        private void txbEmail_Leave(object sender, EventArgs e)
+        {
+            if (txbEmail.Text == string.Empty)
+            {
+                txbEmail.Text = "Nome";
+                txbEmail.ForeColor = Color.Silver;
+            }
+        }
+
+        private void txbSsenha_Enter(object sender, EventArgs e)
+        {
+            if (txbSsenha.Text == "Senha")
+            {
+                txbSsenha.Text = string.Empty;
+                txbSsenha.ForeColor = Color.Black;
+                txbSsenha.PasswordChar = '*';
+            }
+        }
+
+        private void txbSsenha_Leave(object sender, EventArgs e)
+        {
+            if (txbSsenha.Text == string.Empty)
+            {
+                txbSsenha.Text = "Senha";
+                txbSsenha.ForeColor = Color.Silver;
+                txbSsenha.PasswordChar = '\0';
+            }
+
+        }
+
+        //Inicio dos Eventos do botão fechar
+        private void btnFechar_MouseEnter(object sender, EventArgs e)
+        {
+            btnFechar.BackColor = Color.Red;
+        }
+
+        private void btnFechar_MouseLeave(object sender, EventArgs e)
+        {
+            btnFechar.BackColor = Color.Transparent;
+        }
+
+        private void btnFechar_MouseDown(object sender, MouseEventArgs e)
+        {
+            btnFechar.BackColor = Color.Salmon;
+        }
+        //Fim dos Eventos do botão fechar
+
+        //Inicio dos Eventos do botão minimizar
+        private void btnMinimizar_MouseEnter(object sender, EventArgs e)
+        {
+            btnMinimizar.BackColor = Color.Silver;
+        }
+      
+        private void btnMinimizar_MouseDown(object sender, MouseEventArgs e)
+        {
+            btnMinimizar.BackColor = Color.LightGray;
+        }
+
+        private void btnMinimizar_MouseLeave(object sender, EventArgs e)
+        {
+            btnMinimizar.BackColor = Color.Transparent;
+        }
+        //Fim dos Eventos do Botão minimizar
+    }
+}
